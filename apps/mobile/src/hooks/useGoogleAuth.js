56b87af40dcc48204e27bff3fe24efe45cbe80a5
IllonaSab <sabouillona@gmail.com>
2026-08-11 16:51:15 +0200

    feat: android

--- apps/mobile/src/hooks/useGoogleAuth.js
+++ apps/mobile/src/hooks/useGoogleAuth.js
@@ -1,22 +1,15 @@
 import { useEffect } from 'react';
 import * as Google from 'expo-auth-session/providers/google';
 import * as WebBrowser from 'expo-web-browser';
-import { makeRedirectUri } from 'expo-auth-session';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export function useGoogleAuth(onSuccess) {
-  const redirectUri = makeRedirectUri({
-    scheme: 'healis',
-    path: 'redirect',
-  });
-
-  console.log('redirectUri:', redirectUri);
-
   const [request, response, promptAsync] = Google.useAuthRequest({
     iosClientId: '594475500467-s1ua3k4191il83u29g0i3vn7rr8t7scm.apps.googleusercontent.com',
+    androidClientId: '594475500467-692kjh431ivrinhf4j6vfr25f245gfd1.apps.googleusercontent.com',
     webClientId: '594475500467-d5l7bvfod9vqfrhi5ggf5a9o44vall3v.apps.googleusercontent.com',
-    redirectUri,
+    redirectUri: 'https://auth.expo.io/@illona/healis',
   });
 
   useEffect(() => {
